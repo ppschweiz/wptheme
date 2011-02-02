2c91a2c4c058c75c3b0064c3645658e322d0eac0
cockroach <cockroach@0f725ad5-d1be-4695-a374-d43b5ac28db6>
2011-02-02 00:29:40 +0000

    git-svn-id: file:///var/svn/wptheme/branches/0.4@39 0f725ad5-d1be-4695-a374-d43b5ac28db6

--- pps/po/update.sh
+++ pps/po/update.sh
@@ -5,4 +5,13 @@ set -u
 #php trunk/makepot.php wp-theme .. messages.pot
 msgmerge de_DE.po messages.pot -o de_DE.new.po
 mv de_DE.new.po de_DE.po
+
+msgmerge fr_FR.po messages.pot -o fr_FR.new.po
+mv fr_FR.new.po fr_FR.po
+
+msgmerge it_IT.po messages.pot -o it_IT.new.po
+mv it_IT.new.po it_IT.po
+
 msgfmt -o de_DE.mo de_DE.po
+msgfmt -o fr_FR.mo fr_FR.po
+msgfmt -o it_IT.mo it_IT.po
