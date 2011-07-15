71411bb5884c97de491ad14f78b789f35e581d4b
cockroach <cockroach@0f725ad5-d1be-4695-a374-d43b5ac28db6>
2011-07-15 02:38:26 +0000

    Added cookie to hide the menu
    git-svn-id: file:///var/svn/wptheme/trunk@71 0f725ad5-d1be-4695-a374-d43b5ac28db6

--- pps/javascript/pps.js
+++ pps/javascript/pps.js
@@ -1,33 +1,67 @@
-function showMenu() {
-	$$('ul.children').each(function(child) {
-		child.show();
+function setCookie(name, value)
+{
+	var time = new Date();
+	time.setDate(time.getDate() + 365); // days
+	document.cookie=name + '=' + escape(value) + ';path=/';
+}
+
+function getCookie(name)
+{
+	var cookies = document.cookie.split('; ');
+	var value;
+
+	cookies.each(function(cookie)
+	{
+		var data = cookie.split('=');
+
+		if (data[0] == name)
+		{
+			value = data[1];
+		}
 	});
-	$$('ul.sub-menu').each(function(child) {
-		child.show();
-	});
+	return value;
+}
+
+function showMenu()
+{
+	setCookie('hide', 0);
+	doShowMenu();
+	return false;
+}
+
+function doShowMenu()
+{
+	$$('ul.children').invoke('show');
+	$$('ul.sub-menu').invoke('show');
 
 	var img = $('nav-expand');
 	img.src = img.src.replace('down', 'up');
 
-	$('expand').onclick = function() {
-		hideMenu();
-		return false;
-	}
+	$('expand').onclick = hideMenu;
 }
 
-function hideMenu() {
-	$$('ul.children').each(function(child) {
-		child.hide();
-	});
-	$$('ul.sub-menu').each(function(child) {
-		child.hide();
-	});
+function hideMenu()
+{
+	setCookie('hide', 1);
+	doHideMenu();
+	return false;
+}
+
+function doHideMenu()
+{
+	$$('ul.children').invoke('hide');
+	$$('ul.sub-menu').invoke('hide');
 
 	var img = $('nav-expand');
 	img.src = img.src.replace('up', 'down');
 
-	$('expand').onclick = function() {
-		showMenu();
-		return false;
-	}
+	$('expand').onclick = showMenu;
 }
+
+document.observe("dom:loaded", function()
+{
+	if (getCookie('hide') == '1')
+	{
+		doHideMenu();
+	}
+});
